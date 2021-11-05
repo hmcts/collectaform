f49fc42af67f9c3de4d695768182cd8a7dd22e9a
Gudgeon, Alexander <Alexander.Gudgeon@justice.gov.uk>
2021-11-05 15:44:09 +0000

    commit on main

--- app/routes.js
+++ app/routes.js
@@ -10,14 +10,17 @@ router.post('/3COP-NOH', function (req, res) {
   // Check which option the user has selected on the 'submitting' page
   if (varoption === 'cop-noh') {
     // Send user to COP-NOH page
-    res.redirect('/3COP-NOH') } 
+    res.redirect('/3COP-NOH') 
+    } 
   else {
     if (varoption === 'cop-co') {
     // send user to COP-NOHCO page
-    res.redirect('/4COP-NOHCO')}
+    res.redirect('/4COP-NOHCO')
+    }
     else {
     // Send user to supp-docs page
-    res.redirect('/5supp-docs')}
+    res.redirect('/5supp-docs')
+    }
   }
 })
 

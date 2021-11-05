3af082054ff2ca753447c1786070585b452ded63
Gudgeon, Alexander <Alexander.Gudgeon@justice.gov.uk>
2021-11-05 15:38:35 +0000

    commit on main

--- app/routes.js
+++ app/routes.js
@@ -4,28 +4,26 @@ const router = express.Router()
 // Add your routes here - above the module.exports line
 // Run this code after basicinfo to determine next page
 router.post('/3COP-NOH', function (req, res) {
-    // Make a variable and give it the value from 'submitting' question page
-    const varoption = req.session.data['availableforms']
+  // Make a variable and give it the value from 'submitting' question page
+  const varoption = req.session.data.availableforms
 
-    // Check which option the user has selected on the 'submitting' page
-    if (varoption === 'cop-noh') 
-    {
-      // Send user to COP-NOH page
-      res.redirect('/3COP-NOH')
-    } 
-    else 
-        {
-        if(varoption === 'cop-co')
-            {
-            //send user to COP-NOHCO page
-            res.redirect('/4COP-NOHCO')
-            } 
-        else 
-            {
-            // Send user to supp-docs page
-            res.redirect('/5supp-docs')
-            }
-        }
-    })
+  // Check which option the user has selected on the 'submitting' page
+  if (varoption === 'cop-noh') 
+  {
+    // Send user to COP-NOH page
+    res.redirect('/3COP-NOH')
+  } 
+  else {
+      if (varoption === 'cop-co')
+      {
+      // send user to COP-NOHCO page
+      res.redirect('/4COP-NOHCO')
+      }
+      else {
+      // Send user to supp-docs page
+      res.redirect('/5supp-docs')
+    }
+  }
+})
 
 module.exports = router

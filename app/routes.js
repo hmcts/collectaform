2dbb56ddf217f5a1710242608e88bfb0160ff81e
Gudgeon, Alexander <Alexander.Gudgeon@justice.gov.uk>
2021-11-05 14:59:43 +0000

    commit on main

--- app/routes.js
+++ app/routes.js
@@ -3,22 +3,29 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 // Run this code after basicinfo to determine next page
-router.post('/basicinfo', function (req, res) {
-    // Make a variable and give it the value from 'conflict' question page
-    const varconflict = req.session.data.availableforms
-  
-    // Check whether the user has selected Y to having a conflict
-    if (varconflict === 'cop-noh') {
+router.post('/3COP-NOH', function (req, res) {
+    // Make a variable and give it the value from 'submitting' question page
+    const varoption = req.session.data['availableforms']
+
+    // Check which option the user has selected on the 'submitting' page
+    if (varoption === 'cop-noh') 
+    {
       // Send user to COP-NOH page
       res.redirect('/3COP-NOH')
-    } else {
-        if(varconflict === 'cop-co') {
+    } 
+    else 
+        {
+        if(varoption === 'cop-co')
+            {
             //send user to COP-NOHCO page
             res.redirect('/4COP-NOHCO')
-        } else {
-      // Send user to supp-docs page
-      res.redirect('/5supp-docs')
-    }}
-  })
+            } 
+        else 
+            {
+            // Send user to supp-docs page
+            res.redirect('/5supp-docs')
+            }
+        }
+    })
 
 module.exports = router
